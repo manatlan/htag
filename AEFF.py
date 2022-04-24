040bd904573cbacfe2e38f402012d00642ba8af1
manatlan <manatlan@gmail.com>
2022-04-24 18:10:45 +0200

    test

--- AEFF.py
+++ AEFF.py
@@ -1,18 +1,38 @@
 from htag import Tag
 import htbulma as b
 
-class Star(Tag.div): # it's a component ;-)
+class Star1(Tag.div): # it's a component ;-)
+    """ rendering lately """
 
     def init(self,value=0):
         self.value=value
 
-    def more(self,o): self.value+=1
-    def less(self,o): self.value-=1
+    def inc(self,v):
+        self.value+=v
 
     def render(self): # <- ensure dynamic rendering
-        self <= b.Button( "-", _onclick = self.less, _class="is-small" )
-        self <= b.Button( "+", _onclick = self.more, _class="is-small" )
+        self <= b.Button( "-", _onclick = lambda o: self.inc(-1), _class="is-small" )
+        self <= b.Button( "+", _onclick = lambda o: self.inc(1) , _class="is-small" )
         self <= "⭐"*self.value
+
+class Star2(Tag.div): # it's a component ;-)
+    """ rendering immediatly """
+
+    def init(self,value=0):
+        self.value=value
+        self <= b.Button( "-", _onclick = self.bind.inc(-1), _class="is-small" )
+        self <= b.Button( "+", _onclick = self.bind.inc(1) , _class="is-small" )
+        # self <= b.Button( "-", _onclick = lambda o: self.inc(-1), _class="is-small" )
+        # self <= b.Button( "+", _onclick = lambda o: self.inc(1) , _class="is-small" )
+
+        self.content = Tag.span( "⭐"*self.value )
+        self <= self.content
+
+    @Tag.NoRender
+    def inc(self,v):
+        self.value+=v
+        self.content.set( "⭐"*self.value )
+
 
 
 class App(Tag.body):
@@ -29,18 +49,18 @@ class App(Tag.body):
     nav.addEntry("entry 1", lambda: self._mbox.show( "You choose 1" ) )
     nav.addEntry("entry 2", lambda: self._mbox.show( "You choose 2" ) )
 
-    s = Star(12)
-
     tab = b.Tabs()
-    tab.addTab("Tab 1", Tag.b("Set stars") + s )
-    tab.addTab("Tab 2", b.Box("Page 2") )
+    tab.addTab("Tab 1", Tag.b("Set star1s") + Star1(12) )
+    tab.addTab("Tab 2", Tag.b("Set star2s") + Star2(12) )
 
     self <= nav + b.Section( tab )
 
 
 if __name__=="__main__":
+    import logging
+    logging.basicConfig(format='[%(levelname)-5s] %(name)s: %(message)s',level=logging.DEBUG)
 
-  app=App()
+    app=App()
 
-  from htag.runners import BrowserHTTP
-  BrowserHTTP( app ).run()
+    from htag.runners import BrowserHTTP
+    BrowserHTTP( app ).run()
