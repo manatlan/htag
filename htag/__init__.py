456addf822cabe9e06bcccb3cc5d12977e2a7f12
manatlan <manatlan@gmail.com>
2022-05-03 18:43:12 +0200

    0.4.0

--- htag/__init__.py
+++ htag/__init__.py
@@ -9,7 +9,7 @@
 
 from .tag import H,Tag,HTagException
 
-__version__ = "0.3.2" # updated from pypoetry.toml
+__version__ = "0.4.0" # updated from pypoetry.toml
 
 __all__= ["Tag","HTagException"]
 
