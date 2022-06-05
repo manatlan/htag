6b6cd4c227284451d4b05ce39975aaba0ddd5a1a
manatlan <manatlan@gmail.com>
2022-06-05 16:26:51 +0200

    add tests for runners

---
+++ test_runners.py
@@ -0,0 +1,40 @@
+import pytest
+import importlib
+from htag import Tag
+
+class MyApp(Tag.div):
+    def init(self):
+        self <= "Hello World"
+
+
+def test_default():
+    from htag.runners import BrowserHTTP
+    BrowserHTTP( MyApp )
+
+def data_source():
+    for i in  [
+        "BrowserStarletteHTTP",
+        "BrowserStarletteWS",
+        "WebHTTP",
+        "PyWebWiew",
+        "GuyApp",
+        "ChromeApp",
+        "AndroidApp",
+        "BrowserTornadoHTTP",
+        "PyScript",
+    ]:
+        yield i
+
+@pytest.mark.parametrize('my_runner', data_source())
+def test_a_runner( my_runner ):
+
+    mrunners=importlib.import_module("htag.runners")
+
+    if hasattr(mrunners,my_runner):
+        runner=getattr(mrunners,my_runner)
+        runner( MyApp )
+    else:
+        print("can't test %s" % my_runner)
+
+if __name__=="__main__":
+    test_default()
