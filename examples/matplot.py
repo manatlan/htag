7c1042081842f6061e464abae75c18d08dc68996
manatlan <manatlan@gmail.com>
2022-08-16 17:00:38 +0200

    fix: real svg generation

--- examples/matplot.py
+++ examples/matplot.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from htag import Tag
-import matplotlib.pyplot as plt
 import io,base64,random
+import matplotlib.pyplot as plt
 
 class App(Tag):
     def init(self):
-        self.img = Tag.img()
+        self.img = Tag.svg(_viewBox="0 0 1000 1500")
         self.liste=[1, 2, 5, 4]
         
         # create the layout
-        self <= Tag.h3("Matplotlib test") + self.img
-        self <= Tag.button("Add",_onclick=self.add_random)
+        self <= Tag.h3("Matplotlib test" + Tag.button("Add",_onclick=self.add_random))
+        self <= self.img
         
         self.redraw_svg()
 
@@ -22,13 +22,11 @@ class App(Tag):
     def redraw_svg(self):
         plt.plot(self.liste)
         plt.ylabel('some numbers')
+        plt.xlabel('size of the liste')
         
-        data=io.BytesIO()
-        plt.savefig(data)
-        data.seek(0)
-        
-        self.img["src"]=u'data:image/svg;base64,'+base64.b64encode(data.read()).decode()
-
+        with io.StringIO() as fid:
+            plt.savefig(fid,format='svg')
+            self.img.set( fid.getvalue() )
 
 if __name__=="__main__":
     from htag.runners import BrowserHTTP
