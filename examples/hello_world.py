d1a8e62f547c013ec0515594d0c9653d17bfb2c4
manatlan <manatlan@gmail.com>
2022-08-18 18:13:42 +0200

    just a minimal app in dev env, to start coding

---
+++ examples/hello_world.py
@@ -0,0 +1,12 @@
+# the simplest htag'app, in the best env to start development (hot reload/refresh)
+from htag import Tag
+
+class App(Tag.body):
+    def init(self):
+        self += "Hello World"
+
+from htag.runners import DevApp as Runner           # need htag+starlette+uvicorn
+#from htag.runners import BrowserHTTP as Runner     # need htag ONLY !
+app=Runner(App)
+if __name__=="__main__":
+    app.run()
