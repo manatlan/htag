837b29ecd3b489522a1d7726bac32f0c9f4cb598
manatlan <manatlan@gmail.com>
2022-08-30 13:54:33 +0200

    prepare the 0.8.0

--- htag/__init__.py
+++ htag/__init__.py
@@ -9,7 +9,7 @@
 
 from .tag import H,Tag,HTagException
 
-__version__ = '0.7.8' # auto-updated from pypoetry.toml
+__version__ = '0.8.0' # auto-updated from pypoetry.toml
 
 __all__= ["Tag","HTagException"]
 
