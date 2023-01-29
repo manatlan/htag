639f9c7c9b639ce0024e84c3c95d3153291f38f1
manatlan <manatlan@gmail.com>
2023-01-29 10:29:33 +0100

    test chromium-driver with github action

---
+++ GUI_tests.py
@@ -0,0 +1,27 @@
+# taken from https://github.com/jsoma/selenium-github-actions
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+driver.get('http://nytimes.com')
+print(driver.title)
