9a3c7e1939e6655bc67cb6a766799519c44191ce
manatlan <manatlan@gmail.com>
2023-01-30 18:48:26 +0100

    raise up delay

--- selenium/app1.py
+++ selenium/app1.py
@@ -18,11 +18,11 @@ def tests(driver):
     assert "App" in driver.title
 
     driver.find_element(By.XPATH, '//button[text()="click"]').click()
-    driver.implicitly_wait(1) # seconds
+    driver.implicitly_wait(2) # seconds
     driver.find_element(By.XPATH, '//button[text()="click"]').click()
-    driver.implicitly_wait(1) # seconds
+    driver.implicitly_wait(2) # seconds
     driver.find_element(By.XPATH, '//button[text()="click"]').click()
-    driver.implicitly_wait(1) # seconds
+    driver.implicitly_wait(2) # seconds
 
     assert len(driver.find_elements(By.XPATH, '//li'))==3
 
