4e0d5e9807cbe27339ef5b9cf2d2de4196d8ac8b
manatlan <manatlan@gmail.com>
2024-04-11 19:44:24 +0200

    add "htag.ui" (from htagui import *)

---
+++ htag/ui.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# # #############################################################################
+# Copyright (C) 2024 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htag
+# #############################################################################
+from htagui import *
