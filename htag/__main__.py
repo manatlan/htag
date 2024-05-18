8efff3ca9f3d305bf3098d8b5325d0ce54b2aeec
manatlan <manatlan@gmail.com>
2024-05-18 18:06:53 +0200

    add argparse & options in cmdline

--- htag/__main__.py
+++ htag/__main__.py
@@ -18,11 +18,12 @@ class App(Tag.body):
     statics="body {background:#EEE;}"
 
     def init(self):
-        self += "Hello World"
-        self += Tag.button("Say hi", _onclick=self.sayhi)
+        self <= "Hello World"
+        self <= Tag.button("Say hi", _onclick=self.sayhi)
 
-    def sayhi(self,o):
-        self+="hi!"
+    def sayhi(self,ev):
+        self <= "hi!"
+
 
 #=================================================================================
 from htag.runners import Runner
@@ -31,14 +32,31 @@ if __name__ == "__main__":
     Runner(App).run()
 """
 
+import argparse
+
+
 if __name__=="__main__":
-    if len(sys.argv)>1:
+
+    parser = argparse.ArgumentParser(
+        prog="htag",
+        description="""Entrypoint to help you <to create> or <to run> a htag'app.
+If a [file] is given: it will try to run it (using dev mode),
+else it will create an empty htag file. The options are just here for the run mode.
+""",
+    )
+    parser.add_argument('file', nargs='?', help="if present, the htag'file will be runned (reload/debug mode)")
+    parser.add_argument('--host', help='Host listener (default: 127.0.0.1)', default="127.0.0.1")
+    parser.add_argument('--port', help='Port number (default: 8000)', default="8000")
+    parser.add_argument('--gui', help="Automatically open interface in a browser (default!)",action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument('--dev', help="Run in dev mode (reload+debug) (default!)",action=argparse.BooleanOptionalAction, default=True)
+    args = parser.parse_args()
+    if args.file:
         ##########################################################################
         ## run mode
         ##########################################################################
-        htagfile=os.path.realpath(sys.argv[1])
-
+        htagfile=os.path.realpath(args.file)
         try:
+            assert os.path.isfile(htagfile), f"file '{htagfile}' not found"
             from htag.runners import Runner
             import importlib.util
             module_name=os.path.basename(htagfile)[:-3]
@@ -61,13 +79,13 @@ if __name__=="__main__":
                 tagClass=getattr(module,"App")
 
                 # run part (here FULL DEV, and open a tab/browser)
-                app=Runner(tagClass,reload=True,debug=True)
+                app=Runner(tagClass,reload=args.dev,debug=args.dev,host=args.host,port=args.port, interface=1 if args.gui else 0)
                 print(app,"Serving")
                 app.run()
             else:
                 print("ERROR",htagfile,"doesn't contain 'App' (tag class)")
         except Exception as e:
-            print("ERROR",e)
+            print("ERROR",e)        
     else:
         ##########################################################################
         ## create mode
