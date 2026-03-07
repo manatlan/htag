d53de0014d7338daff8a70efe7b4021d54aa9cb4
manatlan <manatlan@gmail.com>
2026-03-07 11:04:06 +0100

    fix simple.py

--- simple.py
+++ simple.py
@@ -1,10 +1,13 @@
 from htag import Tag, ChromeApp, State, prevent, stop
 import time
 
+class Box(Tag.div):
+    styles=".box {border:1px solid green;padding:8px}"
+    def init(self):
+        self._class = "box"
+
 class Showcase(Tag.App):
-    styles='''html,body {margin:0px;padding:0px}
-    .box {border:1px solid green;padding:8px}
-    ''' # scoped
+    styles='''html,body {margin:0px;padding:0px}''' # scoped
     
     def init(self):
         self.s_cpt=State(0)
@@ -14,19 +17,19 @@ class Showcase(Tag.App):
         
 
         with self:
-            with Tag.div(_class="box"):
+            with Box():
                 Tag.button("+1", _onclick=lambda ev: self.s_cpt.set(self.s_cpt.value + 1))
                 Tag.div(self.s_cpt)
 
-            with Tag.div(_class='box'):
+            with Box():
                 Tag.button("+liste", _onclick=lambda ev: self.s_liste.append("b"))
                 Tag.div(self.s_liste)
 
-            with Tag.div(_class='box'):
+            with Box():
                 Tag.button("+dict", _onclick=lambda ev: self.s_dico.update({time.time(): 42}))
                 Tag.div(self.s_dico)
 
-            with Tag.div(_class='box'):
+            with Box():
 
                 # change on each key press
                 Tag.input(_value=self.s_text.value, _onkeyup=lambda ev: self.s_text.set(ev.value))
